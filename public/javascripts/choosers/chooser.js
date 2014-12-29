c6d0f995df4d1e1585f98b3618fc8afe505145fd
Dave Koenig <davidjosephkoenig@gmail.com>
2014-12-28 23:29:56 -0500

    add detect chooser callback

--- public/javascripts/choosers/chooser.js
+++ public/javascripts/choosers/chooser.js
@@ -7,3 +7,49 @@ function replaceChooser(chooser, callback){
     replaceWeight(chooser, callback);
   };
 };
+
+function detectChoosers(){
+	var filter = detectFilter();
+	var displaySelector = detectDisplaySelector();
+	var weight = detectWeight();
+	return {
+		filter: filter,
+		displaySelector: displaySelector,
+		weight: weight
+	};
+};
+
+function detectFilter(){
+  var filter = $('.filter-type').attr('id');
+  if (filter==="usPop-filter") {
+    return "usPop";
+  } else if (filter==="race-filter") {
+    return "race";
+  } else if (filter==="age-filter") {
+    return "age";
+  } else if (filter==="state-filter") {
+    return "state";
+  };
+};
+
+function detectDisplaySelector(){
+  var displayStyle = $('.display-type').attr('id');
+  if (displayStyle==="heatmaps-selector") {
+    return "heatmap";
+  } else if (displayStyle==="markers-selector") {
+    return "marker";
+  } else {
+    return "graph";
+  };
+};
+
+function detectWeight(){
+  var weight = $('.weight-type').attr('id');
+  if (weight===undefined) {
+    return "none";
+  } else if (weight==="usPop-weight") {
+    return "usPop";
+  } else if (weight==="arrests-weight") {
+    return "arrests";
+  };
+};
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,36 +1,5 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoibWFycGJvcnhtYXJycnBib3JycnJyeCIsImEiOiJ3Y0hUd3ZZIn0.VNcoUZ2TFXUuID8JQ2-t2A';
 
-function replaceFilter(filter, callback){
-  if (filter.id != "age-filter") {
-    $('#age-range').children().css({"display":"none"});
-  };
-  if (filter.id != "race-filter") {
-    $('#race-selection').css({"display":"none"});
-  };
-  $('.button-filter').removeClass('filter-type');
-  $(filter).addClass('filter-type');
-  callback();
-};
-
-function replaceSelector(selector, callback){
-  if (selector.id != "heatmaps-selector"){
-    $('#age-range').children().css({"display":"none"});
-  };
-  $('.display-selector').removeClass('display-type');
-  $(selector).addClass('display-type');
-  callback();
-};
-
-function replaceWeight(weight, callback){
-  if ($(weight).hasClass('weight-type')) {
-    $(weight).removeClass('weight-type');
-  } else {
-    $('.button-weight').removeClass('weight-type');
-    $(weight).addClass('weight-type');
-  };  
-  callback();
-};
-
 var defaultLat = 37.78808138412046;
 var defaultLon = -94.39453125;
 var defaultZoom = 4;
@@ -44,15 +13,14 @@ $(function(){
 
   $('.chooser').on('click', function(){
     replaceChooser(this, function(){
-    //   detectChoosers().then(function(){
+      var choosers = detectChoosers();
     //     replaceDisplay().then(function(){
     //       replaceProgram();
     //     })
-    //   })
-    alert('callback!');
+      console.log(choosers);
     })
   });
 
   var killingList = new KillingList();
   killingList.fetch();
-})
+});
