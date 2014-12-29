acfb0889c38a41d74461a033fbf1428fa014b319
Dave Koenig <davidjosephkoenig@gmail.com>
2014-12-28 21:43:19 -0500

    filters replace on click

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,24 @@
 console.log('connected');
 
+function replaceFilter(filter){
+  if (filter.id != "age-filter") {
+    $('#age-range').children().css({"display":"none"});
+  };
+  if (filter.id != "race-filter") {
+    $('#race-selection').css({"display":"none"});
+  };
+  $('.button-filter').removeClass('filter-type');
+  $(filter).addClass('filter-type');
+};
+
 $(function(){
-	$('.chooser').on('click', function(e){
-		alert('you clicked a chooser');
-	})
+	$('.chooser').on('click', function(){
+		if ($(this).hasClass('button-filter')) {
+			replaceFilter(this);
+		} else if ($(this).hasClass('display-selector')) {
+			replaceSelector(this);
+		} else if ($(this).hasClass('button-weight')) {
+			replaceWeight(this);
+		};
+	});
 })
