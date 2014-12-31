49ae3cb3bfa67db126a07804d680ce5e2ac3b5ae
Dave Koenig <davidjosephkoenig@gmail.com>
2014-12-30 20:03:14 -0500

    bugfix chooserListViews won't appear on graphs

--- public/javascripts/choosers/chooserListview.js
+++ public/javascripts/choosers/chooserListview.js
@@ -3,11 +3,6 @@ function updateChooserListviews(choosers){
 	var ageDisplay = $('#age-range').css('display');
 	var genderDisplay = $('#gender-selection').css('display');
 
-	// if the selector is on graph, hide all displays
-	if (choosers['displaySelector'] === "graph"){
-		$('.filter-checkbox-form, #age-range').css({"display":"none"});
-	};
-
 	// if you can't see the racedisplay, and the filter is on race, and the display is not on graph, then show the racedisplay
 	if (raceDisplay === "none"){
 		if (choosers['filter'] === "race"){
@@ -40,27 +35,41 @@ function updateChooserListviews(choosers){
 		}
 	};
 
+	// if the selector is on graph, hide all displays
+	if (choosers['displaySelector'] === "graph"){
+		$('.filter-checkbox-form, #age-range').css({"display":"none"});
+	};
+
 };
 
 $(function(){
 	$('#race-filter').on('click', function(e){
 		e.preventDefault();
-		$('#race-selection').css({"display":"block"});
-		$('#age-range').css({"display":"none"});
-		$('#gender-selection').css({"display":"none"});
+		var choosers = detectChoosers();
+		if (choosers['displaySelector'] != 'graph'){
+			$('#race-selection').css({"display":"block"});
+			$('#age-range').css({"display":"none"});
+			$('#gender-selection').css({"display":"none"});
+		};
 	});
 
 	$('#age-filter').on('click', function(e){
 		e.preventDefault();
-		$('#age-range').css({"display":"block"});
-		$('#race-selection').css({"display":"none"});
-		$('#gender-selection').css({"display":"none"});
+		var choosers = detectChoosers();
+		if (choosers['displaySelector'] != 'graph'){
+			$('#age-range').css({"display":"block"});
+			$('#race-selection').css({"display":"none"});
+			$('#gender-selection').css({"display":"none"});
+		};
 	});
 
 	$('#gender-filter').on('click', function(e){
 		e.preventDefault();
-		$('#gender-selection').css({"display":"block"});
-		$('#race-selection').css({"display":"none"});
-		$('#age-range').css({"display":"none"});
+		var choosers = detectChoosers();
+		if (choosers['displaySelector'] != 'graph'){
+			$('#gender-selection').css({"display":"block"});
+			$('#race-selection').css({"display":"none"});
+			$('#age-range').css({"display":"none"});
+		};
 	});
 });
