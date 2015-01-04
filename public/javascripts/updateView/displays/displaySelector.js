acee750fe9d8939e4496b8aa61f35ed1a6d31b6c
Dave Koenig <davidjosephkoenig@gmail.com>
2015-01-04 17:14:17 -0500

    move functions around to enable deleting of mapdata, heatmaps folders

--- public/javascripts/updateView/displays/displaySelector.js
+++ public/javascripts/updateView/displays/displaySelector.js
@@ -14,3 +14,29 @@ function detectDisplaySelector(){
     return "graph";
   };
 };
+
+function removeExistingMaps(){
+  while (map.hasLayer(geoLayer)) {
+    map.removeLayer(geoLayer);
+  };
+  while (map.hasLayer(heatLayer)) {
+    map.removeLayer(heatLayer);
+  };
+};
+
+function setMapView(stateView){
+  if (stateView === null || stateView === "USA"){
+    $('#display-and-selectors').animate({"height":"31em"},100);
+    $('#program').animate({"height":"17%"},100);   
+    map.setView([defaultLat,defaultLon],defaultZoom);
+  } else {
+    $('#display-and-selectors').animate({"height":"31em"},100);
+    $('#program').animate({"height":"17%"},500);   
+    setMapToStateView(stateView);
+  };
+};
+
+function setMapToStateView(state){
+  var view = stateViews[state];
+  map.setView([view.lat, view.lon],view.zoom);
+};
--- public/javascripts/updateView/heatmap/heatmap.js
+++ public/javascripts/updateView/heatmap/heatmap.js
@@ -48,3 +48,66 @@ function makeHeatmap(data){
     heatLayer.addTo(map);
   };
 };
+
+function selectGradient(stateView){
+  if (stateView === null || stateView === "USA"){
+    return gradientMain;
+  } else {
+    return gradientStateView;
+  }
+};
+
+var gradientMain = {
+  0: 'purple',
+  0.15: 'blue',
+  0.25: 'lightblue',
+  0.35: 'green',
+  0.85: 'yellow',
+  0.995: 'orange',
+  1: 'red'
+};
+
+var gradientStateView = {
+  0: 'purple',
+  0.1: 'blue',
+  0.2: 'green',
+  0.4: 'yellow',
+  1: 'red'
+};
+
+var gradientAgeRange = {
+  0: 'black',
+  0.1: 'purple',
+  0.25: 'blue',
+  0.4: 'green',
+  0.6: 'yellow',
+  1: 'red'
+};
+
+function setMaxZoom(numDatapoints, stateView){
+  if (stateView === null || stateView === "USA") {
+    switch (true) {
+      case (numDatapoints <= 10): return 1;
+      case (numDatapoints > 10 && numDatapoints <= 35): return 4;
+      case (numDatapoints > 35 && numDatapoints <= 130): return 5;
+      case (numDatapoints > 130 && numDatapoints <= 350) : return 6;
+      case (numDatapoints > 350 && numDatapoints <= 750): return 7;
+      case (numDatapoints > 750 && numDatapoints <= 1750): return 8;
+      default: return 10
+    }
+  } else {
+    if (numDatapoints > 25) {
+      return 10;
+    } else if (numDatapoints <= 25 && numDatapoints > 30) {
+      return 9;
+    } else if (numDatapoints <= 30 && numDatapoints > 35) {
+      return 8;
+    } else if (numDatapoints <= 35 && numDatapoints > 40) {
+      return 7;
+    } else if (numDatapoints <= 40 && numDatapoints > 50) {
+      return 5;
+    } else {
+      return 3;
+    }
+  }
+};
