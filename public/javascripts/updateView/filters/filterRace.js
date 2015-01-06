d2f0b0903a07d4b0b6826aed164dc475661dc8fd
Dave Koenig <davidjosephkoenig@gmail.com>
2015-01-06 02:51:07 -0500

    move graph-specific functions from broad filter files to graph-filter files

--- public/javascripts/updateView/filters/filterRace.js
+++ public/javascripts/updateView/filters/filterRace.js
@@ -26,30 +26,6 @@ function reorderRaces(raceArr){
   return reordered;
 };
 
-function abbreviateRace(race,graphBarDirection){
-  if (graphBarDirection === "horizontal"){
-    race = race === "hispanic and/or latin" ? "hispanic" : race;
-    race = race === "alaskan and/or pacific islander" ? "ak / p.i." : race;
-    return race;
-  } else if (graphBarDirection === "vertical"){
-    race = race === "hispanic and/or latin" ? "hispanic/latin" : race;
-    race = race === "alaskan and/or pacific islander" ? "alaskan/p.i." : race;
-    return race;
-  }; 
-};
-
-function expandRace(race,graphBarDirection){
-  if (graphBarDirection === "horizontal"){
-    race = race === "hispanic" ? "hispanic and/or latin" : race;
-    race = race === "ak / p.i." ? "alaskan and/or pacific islander" : race;
-    return race;
-  } else if (graphBarDirection === "vertical"){
-    race = race === "hispanic/latin" ? "hispanic and/or latin" : race;
-    race = race === "alaskan/p.i." ? "alaskan and/or pacific islander" : race;
-    return race;
-  };
-};
-
 var raceColors = {
   "alaskan and/or pacific islander" : "crimson",
   "asian"                           : "darkorange",
--- public/javascripts/updateView/graph/filterAge/graphFilterAge.js
+++ public/javascripts/updateView/graph/filterAge/graphFilterAge.js
@@ -13,3 +13,39 @@ function updateGraphFilterAge(choosers){
 		updateGraphFilterAgeWeightIllness();
 	}
 };
+
+function ageFilterColorArr(){
+  return hexScaler("#FFFFFF","#FFFF00",9)
+  .slice(0,8)
+  .concat(hexScaler("#FFFF00","#FF6600",7))
+  .slice(0,14)
+  .concat(hexScaler("#FF6600","#FF0000",7))
+  .slice(0,20)
+  .concat(hexScaler("#FF0000","#551A8B",7))
+  .concat(hexScaler("#551A8B","#551A8B",14))
+  .concat(hexScaler("#551A8B","#FF0000",8))
+  .slice(0,48)
+  .concat(hexScaler("#FF0000","#FF6600",7))
+  .slice(0,54)
+  .concat(hexScaler("#FF6600","#FFFF00",8))
+  .slice(0,61)
+  .concat(hexScaler("#FFFF00","#FFFFFF",46));
+};
+
+function ageFilterColorArr2(){
+  return hexScaler("#551A8B","#FF0000",9)
+  .slice(0,8)
+  .concat(hexScaler("#FF0000","#FF6600",7))
+  .slice(0,14)
+  .concat(hexScaler("#FF6600","#FFFF00",7))
+  .slice(0,20)
+  .concat(hexScaler("#FFFF00","#FFFFFF",7))
+  .concat(hexScaler("#FFFFFF","#FFFFFF",14))
+  .concat(hexScaler("#FFFFFF","#FFFF00",8))
+  .slice(0,48)
+  .concat(hexScaler("#FFFF00","#FF6600",7))
+  .slice(0,54)
+  .concat(hexScaler("#FF6600","#FF0000",8))
+  .slice(0,61)
+  .concat(hexScaler("#FF0000","#551A8B",46));
+};
