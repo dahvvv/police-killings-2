e77fdf7df7da977e9ee34926de19d43eb852e109
Dave Koenig <davidjosephkoenig@gmail.com>
2015-01-13 00:52:45 -0500

    complete map filtershots weightrace program

--- public/javascripts/updateView/map/filterShots/mFilterShotsWeightNone.js
+++ public/javascripts/updateView/map/filterShots/mFilterShotsWeightNone.js
@@ -12,29 +12,10 @@ function updateMapFilterShotsWeightNone(){
 	});
 	var geoData = dataToGeoData(data);
 	makeMap(geoData);
-	var programIntro = programIntroMapShotsNone(data);
+	var programIntro = programIntroMapShots(data);
 	var programBody = programs.map.shots.none;
 	$("#program").html(programIntro + programBody);
 	$(".top").on("click", function(){
     window.scrollTo(0, 0);
   });
-};
-
-function programIntroMapShotsNone(data){
-	var min = shotsRange().min;
-	var max = shotsRange().max;
-	var timeTimes = max === 1 ? "time" : "times";
-	if (min === 0){
-		if (max === 999){
-			return "<p>All cases in which the number of shots fired by police was recorded.</p>";
-		} else {
-			return "<p class='three-line-opener'>Police have killed someone<br />by shooting " + max + " " + timeTimes + " or fewer<br />in " + data.length + " recorded cases.</p>";
-		};
-	} else {
-		if (max === 999){
-			return "<p class='three-line-opener'>Police have killed someone<br />by shooting " + min + " or more times<br />in " + data.length + " recorded cases.</p>";
-		} else {
-			return "<p class='three-line-opener'>Police have killed someone<br />by shooting between " + min + " and " + max + " times<br />in " + data.length + " recorded cases.</p>";
-		};
-	};
 };
--- public/javascripts/updateView/map/filterShots/mapFilterShots.js
+++ public/javascripts/updateView/map/filterShots/mapFilterShots.js
@@ -17,3 +17,23 @@ var selectMapFilterShotsWeight = {
 		updateMapFilterShotsWeightUnarmed();
 	},
 };
+
+function programIntroMapShots(data){
+	var min = shotsRange().min;
+	var max = shotsRange().max;
+	var timeTimes = max === 1 ? "time" : "times";
+	if (min === 0){
+		if (max === 999){
+			return "<p>All cases in which the number of shots fired by police was recorded.</p>";
+		} else {
+			return "<p class='three-line-opener'>Police have killed someone<br />by shooting " + max + " " + timeTimes + " or fewer<br />in " + data.length + " recorded cases.</p>";
+		};
+	} else {
+		if (max === 999){
+			return "<p class='three-line-opener'>Police have killed someone<br />by shooting " + min + " or more times<br />in " + data.length + " recorded cases.</p>";
+		} else {
+			return "<p class='three-line-opener'>Police have killed someone<br />by shooting between " + min + " and " + max + " times<br />in " + data.length + " recorded cases.</p>";
+		};
+	};
+};
+
