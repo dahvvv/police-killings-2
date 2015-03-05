76cb8e9cdf3880e86134eda0e0a46f41f07c48a7
Dave Koenig <davidjosephkoenig@gmail.com>
2015-03-05 02:01:05 -0500

    fix connection take 2

--- connection.rb
+++ connection.rb
@@ -5,6 +5,6 @@
 
 require 'active_record'
 
-ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"]  + '@127.0.0.1/police_killings_db')
+ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"]  + '@192.241.253.189/police_killings_db')
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)  
