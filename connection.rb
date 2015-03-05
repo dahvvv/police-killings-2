fb2f5f2812476dc221141e97db060dcd31f279aa
Dave Koenig <davidjosephkoenig@gmail.com>
2015-03-05 01:33:38 -0500

    reconfig connection for droplet

--- connection.rb
+++ connection.rb
@@ -1,4 +1,10 @@
-ActiveRecord::Base.establish_connection({
-  adapter: 'postgresql',
-  database: 'police_killings_db'
- });
+# ActiveRecord::Base.establish_connection({
+#   adapter: 'postgresql',
+#   database: 'police_killings_db'
+#  });
+
+require 'active_record'
+
+ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"]  + '@127.0.0.1/police_killings_db')
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)  
